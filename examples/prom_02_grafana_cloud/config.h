bcb6f2a828f918e844a1d71c14a2126ec16c92ac
Richard Hartmann <richih@richih.org>
2021-06-12 22:48:02 +0200

    Fix URL and thus connection issues
    Signed-off-by: Richard Hartmann <richih@richih.org>

--- examples/prom_02_grafana_cloud/config.h
+++ examples/prom_02_grafana_cloud/config.h
@@ -1,7 +1,7 @@
 #define WIFI_SSID     ""
 #define WIFI_PASSWORD ""
 
-#define GC_URL "prometheus-us-central1.grafana.net"
+#define GC_URL "http://prometheus-us-central1.grafana.net"
 #define GC_PATH "/api/prom/push"
 #define GC_PORT 443
 #define GC_USER ""
