ac2ff792f4561a30d9fc5c360e911e7633f15cef
Edward Welch <edward.welch@grafana.com>
2021-06-15 07:17:17 -0400

    more guidance on getting config values

--- examples/prom_02_grafana_cloud/config.h
+++ examples/prom_02_grafana_cloud/config.h
@@ -1,7 +1,8 @@
 #define WIFI_SSID     ""
 #define WIFI_PASSWORD ""
 
-#define GC_URL "prometheus-us-central1.grafana.net"
+// For more information on where to get these values see: https://github.com/grafana/diy-iot/blob/main/README.md#sending-metrics
+#define GC_URL "prometheus-blocks-prod-us-central1.grafana.net"
 #define GC_PATH "/api/prom/push"
 #define GC_PORT 443
 #define GC_USER ""
