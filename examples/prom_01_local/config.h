323c5f008fe178b23edad68954646c57300f5918
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2022-01-18 13:10:26 +0000

    Update PATH define to reflect actual API endpoint (#9)

--- examples/prom_01_local/config.h
+++ examples/prom_01_local/config.h
@@ -2,5 +2,5 @@
 #define WIFI_PASSWORD ""
 
 #define URL "IP_ADDRESS_OR_HOST_OF_SERVER" // "192.168.0.1" or "prometheus.yourdomain.local" No http or https, No port or path
-#define PATH "/api/prom/push"
+#define PATH "/api/v1/write"
 #define PORT 9090
